6ecdc840f60e279f5b06e9abdd8eae9a88fe35cc
rebi13 <rebi13@naver.com>
2024-11-28 23:38:39 +0900

    공통 Modal Stack 관리 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,36 @@ import routePaths from "@/routers";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "@/components/ErrorFallback";
 import { Suspense } from "react";
-import { Loader, MantineProvider, MantineTheme } from "@mantine/core";
+import { Loader, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import { Notifications } from "@mantine/notifications";
 import { Layout } from "@/components/Layout";
 import { globalTheme } from "@/styles/global";
+import { ModalStackManager } from "@/components/Modal";
 
 function App() {
   return (
     <BrowserRouter>
       <MantineProvider theme={globalTheme} withGlobalClasses withStaticClasses>
-        <Notifications />
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <Suspense fallback={<Loader />}>
-            <Layout>
-              <Routes>
-                {routePaths.map((route) => (
-                  <Route
-                    key={route.path}
-                    path={route.path}
-                    element={route.element}
-                  />
-                ))}
-              </Routes>
-            </Layout>
-          </Suspense>
-        </ErrorBoundary>
+        <ModalStackManager>
+          <Notifications />
+          <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <Suspense fallback={<Loader />}>
+              <Layout>
+                <Routes>
+                  {routePaths.map((route) => (
+                    <Route
+                      key={route.path}
+                      path={route.path}
+                      element={route.element}
+                    />
+                  ))}
+                </Routes>
+              </Layout>
+            </Suspense>
+          </ErrorBoundary>
+        </ModalStackManager>
       </MantineProvider>
     </BrowserRouter>
   );
