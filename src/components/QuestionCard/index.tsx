6edebe8daabe6f1f1e4dc746d2f916222c6ea3e2
rebi13 <rebi13@naver.com>
2024-12-06 14:08:30 +0900

    MBTI 색상 상수로 이동

--- src/components/QuestionCard/index.tsx
+++ src/components/QuestionCard/index.tsx
@@ -1,25 +1,7 @@
 import { Question } from "@/@types";
+import { mbtiTypeColor, typeColor } from "@/constants/MBTIColors";
 import useRouter from "@/hooks/useRouter";
-import { themeColor } from "@/styles/color";
 import { Card, Flex, Badge, Progress, Text } from "@mantine/core";
-
-const TypeColor = {
-  energy: themeColor.red?.[4],
-  awareness: themeColor.blue?.[4],
-  judgement: themeColor.yellow?.[4],
-  life: themeColor.green?.[4],
-};
-
-const mbtiTypeColor = {
-  E: themeColor.orange?.[4],
-  I: themeColor.violet?.[4],
-  N: themeColor.teal?.[4],
-  S: themeColor.lime?.[4],
-  T: themeColor.blue?.[4],
-  F: themeColor.pink?.[4],
-  J: themeColor.indigo?.[4],
-  P: themeColor.yellow?.[4],
-};
 
 interface QuestionCardProps {
   question: Question;
@@ -34,7 +16,7 @@ export const QuestionCard = ({ question }: QuestionCardProps) => {
       shadow="sm"
       padding="md"
       radius="md"
-      bg={TypeColor[question.mbtiType]}
+      bg={typeColor[question.mbtiType]}
       h="100%"
       onClick={() => {
         navigateTo(`/question/${question._id}`);
---
+++ src/constants/MBTIColors.ts
@@ -0,0 +1,19 @@
+import { themeColor } from "@/styles/color";
+
+export const typeColor = {
+  energy: themeColor.red?.[4],
+  awareness: themeColor.blue?.[4],
+  judgement: themeColor.yellow?.[4],
+  life: themeColor.green?.[4],
+};
+
+export const mbtiTypeColor = {
+  E: themeColor.orange?.[4],
+  I: themeColor.violet?.[4],
+  N: themeColor.teal?.[4],
+  S: themeColor.lime?.[4],
+  T: themeColor.blue?.[4],
+  F: themeColor.pink?.[4],
+  J: themeColor.indigo?.[4],
+  P: themeColor.yellow?.[4],
+};
