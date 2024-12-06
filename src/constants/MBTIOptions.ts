22619258139b1320da48cd362d7c02738865ff1a
rebi13 <rebi13@naver.com>
2024-12-07 01:43:48 +0900

    타입 정리

--- src/constants/MBTIOptions.ts
+++ src/constants/MBTIOptions.ts
@@ -1,4 +1,3 @@
-type MBTIElementOption = "E" | "I" | "N" | "S" | "T" | "F" | "J" | "P";
 type MBTITypesOption = "energy" | "awareness" | "judgement" | "life";
 export type MBTIDatasOption = {
   energy: string[];
@@ -6,17 +5,6 @@ export type MBTIDatasOption = {
   judgement: string[];
   life: string[];
 };
-
-export const MBTI_ELEMENT_OPTIONS: MBTIElementOption[] = [
-  "E",
-  "I",
-  "N",
-  "S",
-  "T",
-  "F",
-  "J",
-  "P",
-];
 
 export const MBTI_TYPES_OPTIONS: MBTITypesOption[] = [
   "energy",
@@ -33,8 +21,8 @@ export const MBTI_OPTIONS_DATA: MBTIDatasOption = {
 };
 
 export const MBTI_TYPES_VALUE = {
-  energy: { label: "에너지", color: "#EF4444" },
-  awareness: { label: "인식", color: "#F97315" },
-  judgement: { label: "본성", color: "#21C552" },
-  life: { label: "전술", color: "#A755F7" },
+  energy: "에너지",
+  awareness: "인식",
+  judgement: "본성",
+  life: "전술",
 };
